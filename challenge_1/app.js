9267218e6d3c2a436532bc0c1d4f11a1ed9a42f5
Mike <mikeglopez.91@gmail.com>
2019-02-19 23:32:22 -0600

    Allow player turns

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,1 +1,22 @@
-console.log('Hello World!');
+var gameOver = false;
+var isX = true;
+
+var playTurn = function(event) {
+  if (event.target.innerHTML !== '') {
+    return;
+  }
+  event.target.innerHTML = isX ? 'X' : 'O';
+  isX = !isX;
+}
+
+
+
+document.getElementById('r1c1').addEventListener('click', playTurn );
+document.getElementById('r1c2').addEventListener('click', playTurn );
+document.getElementById('r1c3').addEventListener('click', playTurn );
+document.getElementById('r2c1').addEventListener('click', playTurn );
+document.getElementById('r2c2').addEventListener('click', playTurn );
+document.getElementById('r2c3').addEventListener('click', playTurn );
+document.getElementById('r3c1').addEventListener('click', playTurn );
+document.getElementById('r3c2').addEventListener('click', playTurn );
+document.getElementById('r3c3').addEventListener('click', playTurn );
