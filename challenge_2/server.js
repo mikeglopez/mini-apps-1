f2437f3828c72bee6786852b6f5f10600dc5f969
Mike <mikeglopez.91@gmail.com>
2019-02-22 00:44:29 -0600

    Implement basic server, HTML, and package.json

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,9 @@
+require('dotenv').config();
+var express = require('express');
 
+var app = express();
+var port = process.env.PORT || 3000;
 
+app.use(express.static('client'));
+
+app.listen(port, () => {console.log(`Listening on port ${port}`)});
