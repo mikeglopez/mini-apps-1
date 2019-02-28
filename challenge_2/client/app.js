6186d47b86b85f72f0215dd8337df2c8c2fb6ec9
Mike <mikeglopez.91@gmail.com>
2019-02-28 00:33:20 -0600

    Refactor to a single page appliccation using jQuery/AJAX

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,27 @@
 var input = document.getElementById('file');
 input.style.opacity = 0;
+
+$('form').on('submit', (e) => {
+  e.preventDefault();
+
+  var file = document.getElementById('file').files[0];
+  if (file) {
+    var reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = (evt) => {
+      var fileString = evt.target.result;
+      $.ajax({
+        url: '/',
+        method: 'POST',
+        data: { json: fileString },
+        success: function (data) {
+          $('#csv').css('display', 'block');
+          $('#report').text(data);
+        },
+        error: function (error) {
+          console.log('ERROR', error);
+        }
+      });
+    };
+  }
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,24 +1,16 @@
 require('dotenv').config();
 var express = require('express');
 var bodyParser = require('body-parser');
-var generate = require('./middleware/generate');
 var multer = require('multer');
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads');
-  },
-  filename: function (req, file, cb) {
-    cb(null, 'input.json');
-  }
-});
-var upload = multer({ storage: storage });
+var upload = multer();
+// In case you omit the options object, the files
+// will be kept in memory and never written to disk.
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(generate);
 
 app.use(express.static('client'));
 
@@ -29,7 +21,45 @@ app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/client/index.html`);
 });
 
-app.post('/', upload.single('file'), (req, res, next) => {
+
+app.post('/', upload.single('file'), (req, res) => {
   console.log('Received POST request');
-  res.send(req.csv);
+  var json = req.body.json; // JSON string
+  var report = generate(json); // CSV formatted string
+  res.send(report);
 });
+
+var generate = function (jsonStr) {
+  if (typeof jsonStr === 'string') {
+    var jsonObj = JSON.parse(jsonStr); // JSON object
+    var keys = Object.keys(jsonObj);
+    keys.splice(keys.indexOf('children'), 1)
+    keys = keys.join(',');
+    var output = [];
+
+    var checkChildren = function (child) {
+      var vals = [];
+      for (var key in child) {
+        if (key !== 'children') {
+          if (typeof child[key] === 'string' || typeof child[key] === 'number') {
+            vals.push(child[key]);
+          }
+        }
+        if (key === 'children') {
+          output.push(vals.join(','));
+          if (Array.isArray(child.children) && child.children.length) {
+            for (var i = 0; i < child.children.length; i++) {
+              checkChildren(child.children[i]);
+            }
+          }
+        }
+      }
+    }
+
+    checkChildren(jsonObj);
+    output.unshift(keys);
+    output = output.join('\n');
+
+    return output;
+  }
+}
